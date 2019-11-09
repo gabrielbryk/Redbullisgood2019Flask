d95ee7ba7d2729b8d3ab4e4b48ea51e4e164d347
gabrielbryk <gbryk11@gmail.com>
2019-11-09 17:09:11 -0600

    quantapp

--- quantapp.py
+++ quantapp.py
@@ -1,78 +1,80 @@
 from quantconnect.api import Api
-from IPython.core.display import display, HTML
 from sys import getsizeof
+import json
 
-algorithm = 1
-if algorithm == 1:
-    # paired switching algorithm
-    algorithmid = 3383088
-    backtestid = "fce68177edd4066fb4bf54ec9c1e56b5"
+class QuantConnector:
+    def getReport(self,request):
+        data = request.body.decode('utf8').replace("'", '"')
+        data = json.loads(data)
+        algo = data['algo']
+        quant = Api(88707, "84f84df437a2bfe887962a4d09d9a28967c4f69d1297b53ee7e40f49259f1015")
 
-if algorithm == 2:
-    # Mean reversion statistical arbitrage algorithm
-    algorithmid = 3385763
-    backtestid = "b61a205f2037316e957e8e50aa34835e"
+        with open('strategies.json') as json_file:
+            data = json.load(json_file)
 
-if algorithm == 3:
-    # Idiosyncratic Skewness long stocks algorithm
-    algorithmid = 3385792
-    backtestid = "cf2087e486ac230d5a9546f69f5f2917"
+        backtest = data[algo]
 
-
-quant = Api(88707, "84f84df437a2bfe887962a4d09d9a28967c4f69d1297b53ee7e40f49259f1015")
-report = quant.read_backtest(algorithmid, backtestid)
-p = quant.list_projects()
-print(getsizeof(report))
-
-# for data in p["projects"]:
-#     print(data)
-
-if report['success'] == False:
-    print("Report Does Not Exist!")
-    exit()
-
-equityChart = report['result']['Charts']["Strategy Equity"]["Series"]["Equity"]["Values"]
-dailyPerformanceChart = report['result']['Charts']["Strategy Equity"]["Series"]["Daily Performance"]["Values"]
-
-equity = report['result']["RuntimeStatistics"]["Equity"]
-totalFees = report['result']["TotalPerformance"]["TradeStatistics"]["TotalFees"]
-netProfit = report['result']["TotalPerformance"]["TradeStatistics"]["TotalProfitLoss"]
-averageProfit = report['result']["TotalPerformance"]["TradeStatistics"]["AverageProfit"]
-averageLoss = report['result']["TotalPerformance"]["TradeStatistics"]["AverageLoss"]
+        algorithmid = algo['algorithm_id']
+        backtestid = algo['backtest_id']
 
 
-
-def getEquityChart():
-    return equityChart
-
-def getDailyPerformance():
-    return dailyPerformanceChart
-
-def getEquity():
-    return equity
-
-def getTotalFees():
-    return totalFees
-
-def getNetProfit():
-    return netProfit
-
-def getAverageProfit():
-    return averageProfit
-
-def getAverageLoss():
-    return averageLoss
+        report = quant.read_backtest(algorithmid, backtestid)
+        p = quant.list_projects()
 
 
-#result
-    # RollingWindow
-    # TotalPerformance
-        # TradeStatistics
-        # PortfolioStatistics
-        # ClosedTrades
-    # AlphaRuntimeStatistics
-    # Charts
-    # Orders
-    # ProfitLoss
-    # Statistics
-    # RuntimeStatistics
+        backtest['stats'] = report
+
+        return json.dumps(backtest)
+'''
+    # for data in p["projects"]:
+    #     print(data)
+
+    if report['success'] == False:
+        print("Report Does Not Exist!")
+        exit()
+
+    equityChart = report['result']['Charts']["Strategy Equity"]["Series"]["Equity"]["Values"]
+    dailyPerformanceChart = report['result']['Charts']["Strategy Equity"]["Series"]["Daily Performance"]["Values"]
+
+    equity = report['result']["RuntimeStatistics"]["Equity"]
+    totalFees = report['result']["TotalPerformance"]["TradeStatistics"]["TotalFees"]
+    netProfit = report['result']["TotalPerformance"]["TradeStatistics"]["TotalProfitLoss"]
+    averageProfit = report['result']["TotalPerformance"]["TradeStatistics"]["AverageProfit"]
+    averageLoss = report['result']["TotalPerformance"]["TradeStatistics"]["AverageLoss"]
+
+
+
+    def getEquityChart():
+        return equityChart
+
+    def getDailyPerformance():
+        return dailyPerformanceChart
+
+    def getEquity():
+        return equity
+
+    def getTotalFees():
+        return totalFees
+
+    def getNetProfit():
+        return netProfit
+
+    def getAverageProfit():
+        return averageProfit
+
+    def getAverageLoss():
+        return averageLoss
+
+
+    #result
+        # RollingWindow
+        # TotalPerformance
+            # TradeStatistics
+            # PortfolioStatistics
+            # ClosedTrades
+        # AlphaRuntimeStatistics
+        # Charts
+        # Orders
+        # ProfitLoss
+        # Statistics
+        # RuntimeStatistics'''
