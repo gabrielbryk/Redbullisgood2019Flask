da7ca44127c88c47f387f73cf29be1e5db6b5972
Jsilets <jsilets@fwparker.org>
2019-11-09 16:38:00 -0600

    updates

--- quantapp.py
+++ quantapp.py
@@ -1,25 +1,31 @@
 from quantconnect.api import Api
 from IPython.core.display import display, HTML
+from sys import getsizeof
 
+algorithm = 1
 if algorithm == 1:
     # paired switching algorithm
     algorithmid = 3383088
     backtestid = "fce68177edd4066fb4bf54ec9c1e56b5"
 
-else if algorithm == 2:
+if algorithm == 2:
     # Mean reversion statistical arbitrage algorithm
     algorithmid = 3385763
     backtestid = "b61a205f2037316e957e8e50aa34835e"
 
-else if algorithm == 3:
+if algorithm == 3:
     # Idiosyncratic Skewness long stocks algorithm
     algorithmid = 3385792
     backtestid = "cf2087e486ac230d5a9546f69f5f2917"
 
 
-
 quant = Api(88707, "84f84df437a2bfe887962a4d09d9a28967c4f69d1297b53ee7e40f49259f1015")
 report = quant.read_backtest(algorithmid, backtestid)
+p = quant.list_projects()
+print(getsizeof(report))
+
+# for data in p["projects"]:
+#     print(data)
 
 if report['success'] == False:
     print("Report Does Not Exist!")
@@ -34,7 +40,7 @@ netProfit = report['result']["TotalPerformance"]["TradeStatistics"]["TotalProfit
 averageProfit = report['result']["TotalPerformance"]["TradeStatistics"]["AverageProfit"]
 averageLoss = report['result']["TotalPerformance"]["TradeStatistics"]["AverageLoss"]
 
-print(equityChart)
+
 
 def getEquityChart():
     return equityChart
