9f898fec5c186cef600ac13b16d4f7146ea771df
Jsilets <jsilets@fwparker.org>
2019-11-09 19:52:16 -0600

    update

--- quantapp.py
+++ quantapp.py
@@ -6,9 +6,6 @@ class QuantConnector:
     def getReport(algoID, backtestID):
         quant = Api(88707, "84f84df437a2bfe887962a4d09d9a28967c4f69d1297b53ee7e40f49259f1015")
 
-        with open('strategies.json') as json_file:
-            data = json.load(json_file)
-
         backtest = {}
 
         report = quant.read_backtest(algoID, backtestID)
@@ -17,56 +14,63 @@ class QuantConnector:
         backtest['stats'] = report
 
         return backtest
-'''
-    # for data in p["projects"]:
-    #     print(data)
 
-    if report['success'] == False:
-        print("Report Does Not Exist!")
-        exit()
+    
+    def getStrategies():
+        with open('strategies.json') as file:
+            data = file.read()
 
-    equityChart = report['result']['Charts']["Strategy Equity"]["Series"]["Equity"]["Values"]
-    dailyPerformanceChart = report['result']['Charts']["Strategy Equity"]["Series"]["Daily Performance"]["Values"]
+        obj = json.loads(data)
+        return obj
+#     # for data in p["projects"]:
+#     #     print(data)
 
-    equity = report['result']["RuntimeStatistics"]["Equity"]
-    totalFees = report['result']["TotalPerformance"]["TradeStatistics"]["TotalFees"]
-    netProfit = report['result']["TotalPerformance"]["TradeStatistics"]["TotalProfitLoss"]
-    averageProfit = report['result']["TotalPerformance"]["TradeStatistics"]["AverageProfit"]
-    averageLoss = report['result']["TotalPerformance"]["TradeStatistics"]["AverageLoss"]
+#     if report['success'] == False:
+#         print("Report Does Not Exist!")
+#         exit()
 
-print(equityChart)
+#     equityChart = report['result']['Charts']["Strategy Equity"]["Series"]["Equity"]["Values"]
+#     dailyPerformanceChart = report['result']['Charts']["Strategy Equity"]["Series"]["Daily Performance"]["Values"]
 
-    def getEquityChart():
-        return equityChart
+#     equity = report['result']["RuntimeStatistics"]["Equity"]
+#     totalFees = report['result']["TotalPerformance"]["TradeStatistics"]["TotalFees"]
+#     netProfit = report['result']["TotalPerformance"]["TradeStatistics"]["TotalProfitLoss"]
+#     averageProfit = report['result']["TotalPerformance"]["TradeStatistics"]["AverageProfit"]
+#     averageLoss = report['result']["TotalPerformance"]["TradeStatistics"]["AverageLoss"]
 
-    def getDailyPerformance():
-        return dailyPerformanceChart
+# print(equityChart)
 
-    def getEquity():
-        return equity
+#     def getEquityChart():
+#         return equityChart
 
-    def getTotalFees():
-        return totalFees
+#     def getDailyPerformance():
+#         return dailyPerformanceChart
 
-    def getNetProfit():
-        return netProfit
+#     def getEquity():
+#         return equity
 
-    def getAverageProfit():
-        return averageProfit
+#     def getTotalFees():
+#         return totalFees
 
-    def getAverageLoss():
-        return averageLoss
+#     def getNetProfit():
+#         return netProfit
+
+#     def getAverageProfit():
+#         return averageProfit
+
+#     def getAverageLoss():
+#         return averageLoss
 
 
-    #result
-        # RollingWindow
-        # TotalPerformance
-            # TradeStatistics
-            # PortfolioStatistics
-            # ClosedTrades
-        # AlphaRuntimeStatistics
-        # Charts
-        # Orders
-        # ProfitLoss
-        # Statistics
-        # RuntimeStatistics'''
+#     #result
+#         # RollingWindow
+#         # TotalPerformance
+#             # TradeStatistics
+#             # PortfolioStatistics
+#             # ClosedTrades
+#         # AlphaRuntimeStatistics
+#         # Charts
+#         # Orders
+#         # ProfitLoss
+#         # Statistics
+#         # RuntimeStatistics
